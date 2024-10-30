a29bbe13faa81f2172abb18eee5aec503091555a
rolteame <r.s.adebiyi@gmail.com>
2024-10-30 11:00:24 +0100

    Feature: Added condition to image-showing if image exits

--- components/userDetails.vue
+++ components/userDetails.vue
@@ -1,10 +1,10 @@
 <template>
-	<div class="flex items-center">
+	<div class="flex items-center" >
 		<div class="flex flex-col pe-2 text-right">
 			<p class="font-bold text-sm lg:text-lg">{{ auth.user.fullName }}</p>
 			<p class="text-xs lg:text-sm">{{ auth.user.email }}</p>
 		</div>
-		<img :src="auth.user.image" alt="" class="w-10 h-10 rounded-full" />
+		<img :src="auth.user.image" alt="" class="w-10 h-10 rounded-full" v-show="auth.user.image"/>
 	</div>
 </template>
 
